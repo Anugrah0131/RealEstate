d375f93864e063f6ca02d93c8f62bdd9523a12e0
anugrah <anugrah4840@gmail.com>
2026-06-02 11:56:32 +0530

    git push

--- backend/server.js
+++ backend/server.js
@@ -26,20 +26,25 @@ connectDB();
 
 //Middlewares
 const allowedOrigins = [
-    "http://localhost:5173/"
-].filter(Boolean);
+    "http://localhost:5173",
+    "http://localhost:3000"
+];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
-}));
-app.use(express.json())
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+
+            console.log("Blocked Origin:", origin);
+            return callback(new Error("Not allowed by CORS"));
+        },
+        credentials: true,
+    })
+);
+
+app.use(express.json());
 
 //Routes
 app.use("/api/auth", authRouter);
